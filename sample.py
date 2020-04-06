06011ab4cff7cd6b8f654cb2f621576ddaddeb1d
Fistfulofgoat <63265795+Fistfulofgoat@users.noreply.github.com>
2020-04-06 13:57:27 -0800

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,13 @@
+a = int(input("first num"))
+b = int(input("second num"))
+
+if(a>b):
+    print("a is greater than b")
+
+    
+
+elif(a<b):
+    print("a is lower than b")
+else:
+    print("a is equal to b")
+
